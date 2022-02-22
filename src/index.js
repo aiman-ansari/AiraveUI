f83aff5d88ae570fb32f200fcef127343951a093
Aiman Ansari <ansariaiman2000@gmail.com>
2022-02-22 22:36:51 +0530

    Added dark mode and responsive menu

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
-const dark = document.querySelector('#dark');
 const menu = document.querySelector('.menu-icon');
 const icon = document.querySelector('.fa-bars');
 const leftSidebar =  document.querySelector('.left-sidebar');
 const theme = document.querySelector('.toggle-theme');
 const darkTheme = document.querySelector('.fa-moon-o');
-var element = document.body;
-// theme.addEventListener('click',()=>{
-//    if(darkTheme.classList.contains('fa-moon-o')){
-//         darkTheme.classList.replace('fa-moon-o','fa-sun-o');
-//         element.classList.toggle("dark-mode");
-//    }
-//    else{darkTheme.classList.replace('fa-sun-o','fa-moon-o');
-//    element.classList.toggle("dark-mode");}
-    
-// })
+let darkMode = localStorage.getItem('dark-mode');
+const changeToDarkMode = () => {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode','enabled')
+}
+const changeToLightMode = () => {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode',null)
+}
+
+darkMode = localStorage.getItem('darkMode')
+if(darkMode === 'enabled'){
+    darkTheme.classList.replace('fa-moon-o','fa-sun-o')
+    changeToDarkMode()
+}
+theme.addEventListener('click',()=>{
+     darkMode = localStorage.getItem('darkMode')
+    if(darkMode !== 'enabled'){
+        console.log('enabled');
+        darkTheme.classList.replace('fa-moon-o','fa-sun-o')
+        changeToDarkMode()
+    }
+    else{
+        console.log('disabled')
+        changeToLightMode();
+        darkTheme.classList.replace('fa-sun-o','fa-moon-o')
+    }    
+})
 menu.addEventListener('click',()=>{
     if(icon.classList.contains('fa-bars')){
         icon.classList.replace('fa-bars','fa-times');
@@ -21,10 +38,10 @@ menu.addEventListener('click',()=>{
         leftSidebar.style.width = '100vw';
         leftSidebar.style.height = '100vh';
         leftSidebar.style.position = 'fixed';
-    leftSidebar.style.background ="white";
-    leftSidebar.style.display ="flex";
-    leftSidebar.style.flexDirection = 'column';
-    leftSidebar.style.alignItems = 'center';
+        leftSidebar.style.background ="white";
+        leftSidebar.style.display ="flex";
+        leftSidebar.style.flexDirection = 'column';
+        leftSidebar.style.alignItems = 'center';
     }
     else{
         icon.classList.replace('fa-times','fa-bars');
