ea3b30c07dad8b220b14744f220667ea2643f2f7
Aiman <ansariaiman2000@gmail.com>
2022-04-03 02:43:56 -0700

    removed modal file added its content to toast

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,3 +1,5 @@
+
+// Toast
 const toastTopBtn = document.querySelector('#toast-top-btn');
 const toastTop= document.querySelector('.toast-container-top');
 const toastBottom= document.querySelector('.toast-container-bottom');
@@ -5,6 +7,13 @@ const toastDismiss = document.querySelector('.toast-dismiss');
 const toastBottomBtn = document.querySelector('#toast-bottom-btn');
 const toastDismissBtn = document.querySelector('#toast-dismiss-btn');
 const dismissBtn = document.querySelector('#dismiss');
+
+// Modal 
+const modal = document.querySelector('.modal')
+const close = document.querySelector('#close');
+const modalBtn = document.querySelector('#modalBtn')
+
+
 
 toastTopBtn.addEventListener('click',()=>{
     toastTop.style.display = "flex";
@@ -27,3 +36,13 @@ dismissBtn.addEventListener('click',()=>{
     toastDismiss.style.display = "none";
 
 }) 
+
+
+// Modal 
+close.addEventListener('click',()=>{
+    modal.style.display = "none"
+})
+
+modalBtn.addEventListener('click', ()=>{
+    modal.style.display = "flex"
+})
